80f1075f97734b8ba44193db721eaea7582a945e
Martin Blomberg <martin.blomberg@outlook.com>
2023-01-18 14:50:48 +0100

    Reformat

--- src/test/java/org/example/testable/TestabilityTest.java
+++ src/test/java/org/example/testable/TestabilityTest.java
@@ -2,15 +2,11 @@ package org.example.testable;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.internal.matchers.Any;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.verification.VerificationMode;
 
 import java.util.List;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,7 +44,4 @@ class TestabilityTest {
 
         assertEquals(2, logger.count);
     }
-
-
 }
-
